00b5e740664f1b945e0e0cd91870a1b0cf7656d0
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2021-08-08 07:49:03 +0530

    Update etcd-restore.sh

--- etcd-restore.sh
+++ etcd-restore.sh
@@ -3,30 +3,41 @@
 
 ETCDCTL_API=3
 BUCKET=prodetcd
-MINIO_SERVER=172.31.25.231
+MINIO_SERVER=172.31.29.182
 AWS_ACCESS_KEY_ID=admin
 AWS_SECRET_ACCESS_KEY=admin2675
 NOW=$(date +'%d%m%Y-%H%M%S')
 
 DIR="$(pwd)/bkp-$NOW"
 rm -rf "${DIR}"
-mkdir -p "${DIR}"
+mkdir -p "${DIR}/etcd"
 
 # Downloading files from Minio Bucket
 echo "Downloading files from Minio Bucket"
 mc config host add minio http://$MINIO_SERVER:9000 $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY --insecure
 mc mb minio/$BUCKET --insecure
-mc cp -a minio/$BUCKET/ca.crt $DIR/
-mc cp -a minio/$BUCKET/ca.key $DIR/
+mc mb minio/$BUCKET/etcd --insecure
+
+mc find minio/$BUCKET --name "*ca.*" --exec "mc cp {} $DIR/"
+mc find minio/$BUCKET --name "sa.*" --exec "mc cp {} $DIR/"
+
+mc find minio/$BUCKET/etcd/ --name "*ca.*" --exec "mc cp {} $DIR/etcd/"
+
 FILE=`mc ls  minio/$BUCKET/ | grep etcd-snapshot | tail --lines=1 | awk '{print $5}'`
 mc cp -a minio/$BUCKET/$FILE $DIR/
 
-# Restore Certificate
-mkdir -p /etc/kubernetes/pki
-chmod 0644 $DIR/ca.crt
-chmod 0600 $DIR/ca.key
-cp $DIR/ca.crt /etc/kubernetes/pki/ca.crt
-cp $DIR/ca.key /etc/kubernetes/pki/ca.key
+# Restore Kube Certificate
+mkdir -p /etc/kubernetes/pki/etcd
+chmod 0644 $DIR/*.crt
+chmod 0600 $DIR/*.key
+chmod 0600 $DIR/*.pub
+cp $DIR/*ca.* /etc/kubernetes/pki/
+cp $DIR/sa.* /etc/kubernetes/pki/
+
+# Restore ETCD Certificate
+chmod 0644 $DIR/etcd/*.crt
+chmod 0600 $DIR/etcd/*.key
+cp $DIR/etcd/*ca.* /etc/kubernetes/pki/etcd/
 
 # Restore ETCD DB
 mkdir -p /var/lib/etcd
@@ -36,6 +47,9 @@ docker run --rm \
     --env ETCDCTL_API=3 \
     'k8s.gcr.io/etcd-amd64:3.1.12' \
     /bin/sh -c "etcdctl snapshot restore '/backup/$FILE' ; mv /default.etcd/member/ /var/lib/etcd/"
+
+# Remove SNAP file due to ETCD "member unknown"
+rm -rf /var/lib/etcd/member/snap/*.snap
 
 # Initilise Kubernetes
 kubeadm init --ignore-preflight-errors=DirAvailable--var-lib-etcd
